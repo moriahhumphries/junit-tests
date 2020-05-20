61ad03db85805ac36ff25c25bca8ae137037ec88
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-20 15:39:35 -0500

    completed tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -3,6 +3,38 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StudentTest {
+    @Test
+    public void testCreateStudent(){
+        Student Moriah = new Student(123, "Moriah");
+        Student Kerrigan = null;
+        assertNull(Kerrigan);
+        assertNotNull(Moriah);
+    }
 
+    @Test
+    public void testStudentFields(){
+        Student Moriah = new Student(123, "Moriah");
+//        assertSame(123, Moriah.getId());
+//        assertSame("Moriah", Moriah.getName());
+//        assertSame(123, Moriah.getGrades().size());
+    }
+
+
+    @Test
+    public void testAddGrade(){
+        Student Moriah = new Student(123, "Moriah");
+        Moriah.addGrade(92);
+        assertSame(92, Moriah.getGrades().get(0));
+        Moriah.addGrade(82);
+        assertSame(82, Moriah.getGrades().get(1));
+    }
+
+    @Test
+    public void testAverageGrade(){
+        Student Moriah = new Student(123, "Moriah");
+        Moriah.addGrade(76);
+        Moriah.addGrade(90);
+        assertEquals(83, Moriah.getGradeAverage(), 0);
+    }
 
 }
